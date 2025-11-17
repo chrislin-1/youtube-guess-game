1f782ef429a0f3b7fe9c2f1f3db93709bae38801
c5system <c5system@Studio-G-Renderer.local>
2025-11-17 17:24:27 -0500

    trying to change to production environment

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@
 import knex from "knex";
 import config from "./knexfile.js";
 
-const environment = process.env.NODE_ENV || "development";
+const environment = process.env.NODE_ENV || "production";
 const db = knex(config[environment]);
 
 export default db;
