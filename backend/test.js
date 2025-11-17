d816c13826f29f73380282be31954ba45846ae9a
c5system <c5system@Studio-G-Renderer.local>
2025-11-17 17:29:20 -0500

    trying to fix knex

--- backend/test.js
+++ backend/test.js
@@ -6,6 +6,8 @@ async function testConnection() {
     console.log("✅ DB connection successful:", result.rows);
     process.exit(0);
   } catch (err) {
+    console.log("the database url is....", process.env.DATABASE_URL);
+    console.log("the production environment is", process.env.NODE_ENV);
     console.error("❌ DB connection failed:", err);
     process.exit(1);
   }
